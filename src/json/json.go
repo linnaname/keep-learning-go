7f5723ff801f86834adc713d42689d2dcc60e6d7
季珂 <bolin9212@163.com>
2020-12-14 21:39:00 +0800

    learn about runtime and json

---
+++ src/json/json.go
@@ -0,0 +1,66 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"strings"
+)
+
+type ColorGroup struct {
+	ID     int
+	Name   string
+	Colors []string
+}
+
+type Animal struct {
+	Name  string
+	Order string
+}
+
+type Message struct {
+	Name, Text string
+}
+
+func main() {
+	group := ColorGroup{
+		ID:     1,
+		Name:   "Reds",
+		Colors: []string{"Crimson", "Red", "Ruby", "Maroon"},
+	}
+	b, err := json.Marshal(group)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Println(string(b))
+
+	var jsonBlob = []byte(`[
+        {"Name": "Platypus", "Order": "Monotremata"},
+        {"Name": "Quoll",    "Order": "Dasyuromorphia"}
+    ]`)
+	var animals []Animal
+	e := json.Unmarshal(jsonBlob, &animals)
+	if err != nil {
+		fmt.Println("error:", e)
+	}
+	fmt.Println(animals)
+
+	const jsonStream = `
+        {"Name": "Ed", "Text": "Knock knock."}
+        {"Name": "Sam", "Text": "Who's there?"}
+        {"Name": "Ed", "Text": "Go fmt."}
+        {"Name": "Sam", "Text": "Go fmt who?"}
+        {"Name": "Ed", "Text": "Go fmt yourself!"}
+    `
+	dec := json.NewDecoder(strings.NewReader(jsonStream))
+	for {
+		var m Message
+		if err := dec.Decode(&m); err == io.EOF {
+			break
+		} else if err != nil {
+			log.Fatal(err)
+		}
+		fmt.Printf("%s: %s\n", m.Name, m.Text)
+	}
+}
---
+++ src/runtime/rt.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"runtime"
+	"runtime/debug"
+)
+
+func main() {
+	//注意这个会STW，所以设置应该尽早进行
+	runtime.GOMAXPROCS(1)
+	fmt.Println("cpus:", runtime.NumCPU())
+	fmt.Println("goroot:", runtime.GOROOT())
+	fmt.Println("GOOS:", runtime.GOOS)
+	fmt.Println("NumGoroutine:", runtime.NumGoroutine())
+
+	go say("world")
+	say("hello")
+	//从注释来看这个GC是立即执行的，和Java的并不相同
+	runtime.GC()
+
+	debug.SetGCPercent(100)
+	debug.SetMaxStack(1000)
+	debug.SetMaxThreads(100)
+}
+
+func say(s string) {
+	for i := 0; i < 2; i++ {
+		runtime.Gosched()
+		fmt.Println(s)
+	}
+}
